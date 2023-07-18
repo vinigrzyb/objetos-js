3e4821c8c4b5869d725845fabfa8aa312c09c7f2
vinigrzyb <vinicius.grzyb@gmail.com>
2023-07-18 19:19:59 -0300

    obj

---
+++ acessando-dados.js
@@ -0,0 +1,15 @@
+const cliente = {
+    nome: "andre",
+    idade: 32,
+    cpf: "123456789",
+    email: "andre@exemplo.com"
+};
+
+// acessando dados com .
+console.log(`O nome do cliente é ${cliente.nome}, seu email é ${cliente.email}.`); // "." permite acessar dados expecíficos do objeto
+console.log(`OS 3 primeitos digitos do cpf são ${cliente.cpf.substring(0, 3)}.`); // .substring permite usar apenas uma ´parte de uma string
+
+// acessando dacos com []
+console.log(`O nome do cliente é ${cliente["nome"]}, seu email é ${cliente["email"]}.`)
+
+
---
+++ objeto.js
@@ -0,0 +1,4 @@
+const pessoa = {
+    nome: "josé", // chave: nome, dado: "josé"
+    idade: 32;
+}
